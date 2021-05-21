afa400342b93742cb7699f9b4d75325072e5f080
Admin <codewithbishal@gmail.com>
2021-05-21 21:08:33 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+if(!defined('Myfooter')){
+	header('location: 404');
+	exit();
+}
+?>
+
+
+    <div class="cookie-container">
+        <p>
+          We use cookies in this website to give you the best experience on our
+          site and show you relevant ads. To find out more, read our
+          <a href="https://codewithbishal.com/privacy-policy-cwb">privacy policy</a> and <a href="cookies-cwb">cookie policy</a>.
+        </p>
+  
+        <button class="cookie-btn">
+          Okay
+        </button>
+      </div>
+  
+<hr>
+<!-- Footer -->
+  <footer class="page-footer font-small unique-color-dark">
+  <hr>
+  
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+    
+      <!-- Grid row -->
+      <div class="row mt-3">
+    
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+    
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">About US</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Code With Bishal is a helping site for web developers, covering  many aspects of web development, it was created on September 2020.</p>
+    
+      </div>
+      <!-- Grid column -->
+    
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+    
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+        <a href="https://codewithbishal.com/">Home</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/about-us">About US</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/contact-us">Contact US</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/posts">Example</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/user-contribution">Service</a>
+        </p>
+    
+      </div>
+      <!-- Grid column -->
+    
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+    
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+        <a href="https://codewithbishal.com/privacy-policy-cwb">Privacy Policy</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/terms-and-conditions">Terms & Conditions</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/cookies-cwb">Cookie Consent</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/search?search=html+and+css">HTML + CSS</a>
+        </p>
+        <p>
+        <a href="https://codewithbishal.com/search?search=html%2C+css+and+js">HTML + CSS +JS</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+    
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+    
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+        <i class="fas fa-home mr-3"></i> North - East, India</p>
+        <p>
+        <i class="fas fa-envelope mr-3"></i><a href="mailto:help@codewithbishal.com">help@codewithbishal.com</a></p>
+        <p>
+    
+      </div>
+      <!-- Grid column -->
+    
+      </div>
+      <!-- Grid row -->
+    
+    </div>
+    <!-- Footer Links -->
+    <hr>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 - <?php echo date('Y'); ?> Copyright:
+      <a href="https://codewithbishal.com/"> codewithbishal.com</a>
+    </div>
+<!-- <script src="js/oc.js" defer></script> -->
+<script src="https://codewithbishal.com/js/mini/live-chat.min.js" defer></script>
+<script src="https://codewithbishal.com/js/mini/enter13.min.js" defer></script>
+<script src="https://codewithbishal.com/js/mini/main.min.js" defer></script>
+<script src="https://codewithbishal.com/js/mini/defer-img.min.js" defer></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+	<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "9a1cc713-768c-4028-aaed-cfe8b78b838a",
+    });
+  });
+</script>
+    </footer>
+    <!-- Footer -->
---
+++ index.php
@@ -0,0 +1,237 @@
+<?php
+session_start();
+session_regenerate_id();
+$search = "<a class='btn btn-primary' href='vaccine'>Search for a new pin code</a>";
+$search_new = "<a class='btn btn-primary' href='vaccine'>Refresh</a>";
+$date = date("d-m-y");
+
+
+if(isset($_POST['submit'])){
+  $Pincode = htmlspecialchars($_POST['pincode']);
+  // Api link starts 
+    $link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=";
+    $link .= $Pincode;
+    $link .= "&date=";
+    $link .= $date;
+ }
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $link,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+$response = json_decode($response, true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Check your nearest Vaccination Center and Slot Availability">
+    <meta name="robots" content="index, follow">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link rel="canonical" href="https://codewithbishal.com/vaccine">
+	  <link rel="apple-touch-icon" sizes="180x180" href="https://codewithbishal.com/logo/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="https://codewithbishal.com/logo/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="https://codewithbishal.com/logo/favicon-16x16.png"><link rel="manifest" href="https://codewithbishal.com/logo/site.webmanifest">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-181718210-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-181718210-2');
+</script>
+    <title>Vaccine | Code With Bishal</title>
+    <style>
+    .cookie-btn{
+        background-color: rgba(54, 54, 238, 0.897) !important;
+    }
+    [disabled="disabled"]{
+        cursor:not-allowed;
+    }
+    hr, #social{
+        display: none !important;
+    }
+    footer{
+            margin-top: 30% !important;
+        }
+    #data{
+      display: none;
+    }
+    div#scroll{
+      display: none;
+    }
+    @media only screen and (max-width: 600px) {
+      div#scroll{
+      display: block;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+    }
+    #data::-webkit-scrollbar {
+      all:unset !important;
+    }
+}
+    </style>
+</head>
+<body>
+<header class="text-gray-700 body-font">
+<?php
+define('Myheader', TRUE);
+require ('header.php');
+?>
+</header>
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Vaccination</h1>
+<?php 
+if($Pincode){
+  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  Showing Results for '.$Pincode.' &nbsp; &nbsp; '.$search.'
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>';
+echo '<style> #data{display:block;} #pin-code-search, .g-recaptcha{display:none;} .px-5, .py-24{padding: 0;}</style>';
+echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  To refresh the Search Results please click refresh&nbsp; &nbsp; '.$search_new.'
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>';
+}
+?>
+<?php if(isset($failed)){
+echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+'.$failed.'
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>';
+}
+?>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base" id="text-description">Check your nearest Vaccination Center and Slot Availability</p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base" id="text-description">Note: All the data are being fetched from Co-WIN public API</p>
+    </div>
+    <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end" id="pin-code-search">
+      <div class="relative flex-grow w-full">
+      <form action="" method="POST" id="pin" autocomplete="off" class="needs-validation" novalidate>
+        <input type="number" id="pincode" name="pincode" class="form-control" placeholder="Please enter your Pin code here" min="100000" max="999999" required autofocus>
+        <div class="invalid-feedback">Please provide a valid Pincode.</div>
+      </div>
+      <div class="modal fade" id="gcaptcha" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Please fill the CAPTCHA to continue</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="g-recaptcha" data-sitekey="6LdhON0aAAAAAIp6c5VdZhntOAymRET1POsnDMBK" data-callback="enableBtn" data-expired-callback="disableBtn"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary" name="submit" id="submit" title="Please fill the CAPTCHA to continue" disabled="disabled">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+      <button type="button" data-toggle="modal" data-target="#gcaptcha" class="btn btn-primary btn-lg">Search</button>
+    </div>
+    </form>
+  </div>
+</section>
+<section id="data">
+<div class="container my-4">
+<div class="text-center" id="scroll">
+  <i class="fas fa-arrow-left fa-3x"></i>
+  <h1>Scroll</h1>
+  <i class="fas fa-arrow-right fa-3x"></i>
+</div>
+    <table class="table table-bordered text-center table-hover" id="myTable">
+      <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Location</th>
+          <th scope="col">Available Vaccine</th>
+          <th scope="col">Vaccine</th>
+          <th scope="col">Minimum Age Limit</th>
+        </tr>
+      </thead>
+      <tbody>
+<?php
+foreach($response['centers'] as $list ){
+  foreach($list['sessions'] as $value) {
+          $book = '<a class="btn btn-success" href="https://www.cowin.gov.in/home" title="Click to register" target="_blank" rel="noopener noreferrer">&nbsp';
+          $book .= $value['available_capacity'];
+          $book .= "</a>";
+          if($value['available_capacity'] =='0'){
+            $available_capacity = "<span class='btn btn-danger'>0 (All Booked)</span>";
+          }
+          else{
+            $available_capacity = $book;
+          }
+          echo "
+          <tr>
+          <td>".$value['date']."</td>
+          <td>".$list['name']."</td>
+          <td>".$available_capacity."</td>
+          <td>".$value['vaccine']."</td>
+          <td>".$value['min_age_limit']."</td>
+          </tr>
+          ";
+        }
+      }
+      ?>
+      </tbody>
+    </table>
+    <div class="text-center" id="scroll">
+      <i class="fas fa-arrow-left fa-3x"></i>
+      <h1>Scroll</h1>
+      <i class="fas fa-arrow-right fa-3x"></i>
+    </div>
+  </div>
+  <hr>
+
+</section>
+<script>
+if($(window).width()<768){
+    $("#myTable").addClass("table-responsive");
+} 
+else {
+    $("#myTable").removeClass("table-responsive");
+}
+</script>
+<script src="js/mini/form-val.min.js" defer></script>
+<script src="js/mini/autosave.min.js"></script>
+<script src="js/mini/captcha.min.js" defer></script>
+<script src="js/mini/load.min.js" defer></script>
+<script>
+  saveStorage('#pin');
+</script>
+<footer class="page-footer font-small unique-color-dark">
+<?php
+define('Myfooter', TRUE);
+require ('footer.php');
+?>
+</footer>
+<hr>
+</body>
+</html>
