50855cda529bd232a53abed533bee2146b74c03b
Admin <codewithbishal@gmail.com>
2021-05-24 12:14:47 +0530

    update

--- index.php
+++ index.php
@@ -41,8 +41,8 @@ if(isset($_POST['submit'])){
     
     $response = curl_exec($curl);
     
-    // curl_close($curl);
-    // $response = json_decode($response, true);
+    curl_close($curl);
+    $response = json_decode($response, true);
  }
 }
 
@@ -181,7 +181,7 @@ if(isset($_COOKIE['Search'])){
   <h1>Scroll</h1>
   <i class="fas fa-arrow-right fa-3x"></i>
 </div>
-    <table class="table table-bordered text-center table-hover" id="myTable"><?php print_r($response); ?>
+    <table class="table table-bordered text-center table-hover" id="myTable">
       <thead>
         <tr>
           <th scope="col">Date</th>
